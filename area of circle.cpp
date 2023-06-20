d1aecbfb987ab7d89becd26bb39f268a56d15f07
MD.Mridul Ali <136635472+md-mridul-ali@users.noreply.github.com>
2023-06-20 23:57:16 +0600

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+float r,area;
+
+cout<<"Enter the value of radius in circle:";
+cin>>r;
+
+area=3.1416*r*r;
+
+cout<<"Area of circle="<<area<<endl;
+
+return 0;
+}
