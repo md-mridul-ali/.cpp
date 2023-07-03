888984a69a64ec87c3ae70003f0330d579b45929
MD.Mridul Ali <136635472+md-mridul-ali@users.noreply.github.com>
2023-07-03 14:23:28 +0600

    cpp file

---
+++ calculate the volume of a sphere.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int rad;
+float volsp;
+
+cout<<"Input the radius of a sphere : ";
+cin>>rad;
+cout<<"\n";
+
+volsp=(4*3.14*rad*rad*rad)/3;
+cout<<"The volume of a sphere is : "<<volsp<< endl;
+
+return 0;
+}
